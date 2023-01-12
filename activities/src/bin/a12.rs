d695d3de2d8bda47cdef6bd98aa0e88a654a2ce9
Kittisak Phetrungnapha <cs.sealsoul@gmail.com>
2023-01-12 15:58:36 +0700

    Finish activity a12

--- activities/src/bin/a12.rs
+++ activities/src/bin/a12.rs
@@ -10,4 +10,35 @@
 // * Implement functionality on the box struct to create a new box
 // * Implement functionality on the box struct to print the characteristics
 
-fn main() {}
+enum Color {
+    Red,
+    Blue
+}
+
+struct Box {
+    dimensions: f64,
+    weight: f64,
+    color: Color
+}
+
+impl Box {
+    fn new(dimensions: f64, weight: f64, color: Color) -> Self {
+        Self {
+            dimensions,
+            weight,
+            color
+        }
+    }
+
+    fn print_characteristics(&self) {
+        match self.color {
+            Color::Red => println!("{:?}, {:?}, Red", self.dimensions, self.weight),
+            Color::Blue => println!("{:?}, {:?}, Blue", self.dimensions, self.weight)
+        }
+    }
+}
+
+fn main() {
+    let item = Box::new(10.0, 50.0, Color::Red);
+    item.print_characteristics();
+}
