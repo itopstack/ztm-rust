292be046d68133b70313f991a513c1f0f87c13f3
Kittisak Phetrungnapha <cs.sealsoul@gmail.com>
2023-01-12 16:46:16 +0700

    Finish a14

--- activities/src/bin/a14.rs
+++ activities/src/bin/a14.rs
@@ -11,4 +11,45 @@
 // * Use an if expression to determine which person's info should be printed
 // * The name and colors should be printed using a function
 
-fn main() {}
+struct Person {
+    color: String,
+    name: String,
+    age: i32
+}
+
+impl Person {
+    fn print_name(&self) {
+        println!("{:?}", self.name);
+    }
+
+    fn print_color(&self) {
+        println!("{:?}", self.color);
+    }
+}
+
+fn main() {
+    let persons = vec![
+        Person {
+            color: "Red".to_owned(),
+            name: "Bob".to_owned(),
+            age: 8
+        },
+        Person {
+            color: "Green".to_owned(),
+            name: "Alice".to_owned(),
+            age: 6
+        },
+        Person {
+            color: "Blue".to_owned(),
+            name: "Mark".to_owned(),
+            age: 24
+        }
+    ];
+
+    for person in persons {
+        if person.age <= 10 {
+            person.print_color();
+            person.print_name()
+        }
+    }
+}
