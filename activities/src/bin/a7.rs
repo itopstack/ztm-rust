258d170c66b9b42878409922c9a8283ca42a523f
Kittisak Phetrungnapha <cs.sealsoul@gmail.com>
2023-01-12 11:20:24 +0700

    Finish activity a7

--- activities/src/bin/a7.rs
+++ activities/src/bin/a7.rs
@@ -10,4 +10,21 @@
 // * Use a match expression to determine which color
 //   name to print
 
-fn main() {}
+enum Color {
+    Red,
+    Green,
+    Blue
+}
+
+fn show_color(color: Color) {
+    match color {
+        Color::Red => println!("Red!"),
+        Color::Green => println!("Green!"),
+        Color::Blue => println!("Blue!")
+    }
+}
+
+fn main() {
+    let color = Color::Red;
+    show_color(color);
+}
