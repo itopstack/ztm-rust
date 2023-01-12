fb83b1f689ad20decd99fa3e329e36392789488d
Kittisak Phetrungnapha <cs.sealsoul@gmail.com>
2023-01-12 17:41:09 +0700

    Finish a15

--- activities/src/bin/a15.rs
+++ activities/src/bin/a15.rs
@@ -11,4 +11,24 @@
 // * Create one of each ticket and place into a vector
 // * Use a match expression while iterating the vector to print the ticket info
 
-fn main() {}
+enum Ticket {
+    Backstage(f64, String),
+    Vip(f64, String),
+    Standard(f64)
+}
+
+fn main() {
+    let tickets = vec![
+        Ticket::Backstage(100.0, "Bob".to_owned()),
+        Ticket::Vip(50.0, "Alice".to_owned()),
+        Ticket::Standard(10.0)
+    ];
+
+    for ticket in tickets {
+        match ticket {
+            Ticket::Backstage(price, name) => println!("Backstage: {:?}, price: {:?}", name, price),
+            Ticket::Vip(price, name) => println!("Vip: {:?}, price: {:?}", name, price),
+            Ticket::Standard(price) => println!("Standard price: {:?}", price)
+        }
+    }    
+}
