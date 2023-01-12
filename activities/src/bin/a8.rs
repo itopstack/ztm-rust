cea3b4dedc0730efd59d701f907c048296ebccd8
Kittisak Phetrungnapha <cs.sealsoul@gmail.com>
2023-01-12 11:57:17 +0700

    Finish activity a8

--- activities/src/bin/a8.rs
+++ activities/src/bin/a8.rs
@@ -9,4 +9,28 @@
 // * Use a function to print out the drink flavor and ounces
 // * Use a match expression to print the drink flavor
 
-fn main() {}
+enum Drink {
+    Coffee,
+    Tea
+}
+
+struct Item {
+    drink: Drink,
+    fluid_ounce: f64
+}
+
+fn show_item(item: Item) {
+    match item.drink {
+        Drink::Coffee => println!("Coffee!"),
+        Drink::Tea => println!("Tea!")
+    }
+    println!("{:?}", item.fluid_ounce);
+}
+
+fn main() {
+    let item = Item {
+        drink: Drink::Coffee,
+        fluid_ounce: 10.5
+    };
+    show_item(item);
+}
