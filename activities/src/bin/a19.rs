31c71f254dbe8576bb650ee2c1a98378ec65532b
Kittisak Phetrungnapha <cs.sealsoul@gmail.com>
2023-01-12 23:29:39 +0700

    Finish a19

--- activities/src/bin/a19.rs
+++ activities/src/bin/a19.rs
@@ -13,4 +13,22 @@
 // Notes:
 // * Use a HashMap for the furniture store stock
 
-fn main() {}
+use std::collections::HashMap;
+
+fn main() {
+    let mut map = HashMap::new();
+    map.insert("Chair", 5);
+    map.insert("Beds", 3);
+    map.insert("Tables", 2);
+    map.insert("Couches", 0);
+
+    let mut count = 0;
+    for (name, total) in map.iter() {
+        match total {
+            0 => println!("{:?}: {:?}", name, "out of stock"),
+            _ => println!("{:?}: {:?}", name, total)
+        }
+        count += total;
+    }
+    println!("total items: {:?}", count);
+}
