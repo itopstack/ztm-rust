156e7b97a8f7e1a12dc0e47ae3048295283816c7
Kittisak Phetrungnapha <cs.sealsoul@gmail.com>
2023-01-12 15:28:16 +0700

    Finish activity a11

--- activities/src/bin/a11.rs
+++ activities/src/bin/a11.rs
@@ -9,4 +9,24 @@
 // * Create a function to display the quantity, with the struct as a parameter
 // * Create a function to display the id number, with the struct as a parameter
 
-fn main() {}
+struct Grocery {
+    id_number: i32,
+    quantity: i32
+}
+
+fn display_id(grocery: &Grocery) {
+    println!("{:?}", grocery.id_number);
+}
+
+fn display_quantity(grocery: &Grocery) {
+    println!("{:?}", grocery.quantity);
+}
+
+fn main() {
+    let grocery = Grocery {
+        id_number: 1,
+        quantity: 10
+    };
+    display_id(&grocery);
+    display_quantity(&grocery);
+}
