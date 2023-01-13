26a1febe76a68c1f99baa42d87cf23b345a9aded
Kittisak Phetrungnapha <cs.sealsoul@gmail.com>
2023-01-13 10:29:30 +0700

    Finish a24

--- activities/src/bin/a24.rs
+++ activities/src/bin/a24.rs
@@ -10,4 +10,13 @@
 
 fn main() {
     let data = vec![1, 2, 3, 4, 5];
+    let nums: Vec<_> = data
+                .iter()
+                .map(|num| num * 3)
+                .filter(|num| num > &10)
+                .collect();
+    
+    for num in nums {
+        println!("{:?}", num);
+    }
 }
