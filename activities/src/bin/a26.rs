ad6b3fca16accf28ab4202fd5798b503283f02ee
Kittisak Phetrungnapha <cs.sealsoul@gmail.com>
2023-01-16 22:17:55 +0700

    Finish a26

--- activities/src/bin/a26.rs
+++ activities/src/bin/a26.rs
@@ -8,4 +8,9 @@
 // * (OPTIONAL) Read the documentation section `Formatting and Parsing`
 //   for examples on how to create custom time formats
 
-fn main() {}
+use chrono::prelude::*;
+
+fn main() {
+    let dt = Utc.with_ymd_and_hms(2014, 11, 28, 12, 0, 9).unwrap();
+    println!("{:?}", dt.to_string());
+}
