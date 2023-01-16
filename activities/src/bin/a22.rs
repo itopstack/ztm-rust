978cf142f279842a1ae4159f3c748a46792606e4
Kittisak Phetrungnapha <cs.sealsoul@gmail.com>
2023-01-16 18:38:50 +0700

    Finish a22

--- activities/src/bin/a22.rs
+++ activities/src/bin/a22.rs
@@ -23,12 +23,62 @@ fn clamp(n: i32, lower: i32, upper: i32) -> i32 {
 
 /// Divides a and b.
 fn div(a: i32, b: i32) -> Option<i32> {
+    if b == 0 {
+        return None;
+    }
     Some(a / b)
 }
 
 /// Takes two strings and places them immediately one after another.
 fn concat(first: &str, second: &str) -> String {
-    format!("{} {}", first, second)
+    format!("{}{}", first, second)
 }
 
 fn main() {}
+
+#[cfg(test)]
+mod test {
+    use crate::*;
+
+    #[test]
+    fn test_clamp_lower_bound() {
+        let result = clamp(-1, 0, 99);
+        let expected = 0;
+        assert_eq!(result, expected, "n should be inbound");
+    }
+
+    #[test]
+    fn test_clamp_upper_bound() {
+        let result = clamp(100, 0, 99);
+        let expected = 99;
+        assert_eq!(result, expected, "n should be inbound");
+    }
+
+    #[test]
+    fn test_clamp_middle() {
+        let result = clamp(50, 0, 99);
+        let expected = 50;
+        assert_eq!(result, expected, "n should be inbound");
+    }
+
+    #[test]
+    fn test_divide_some() {
+        let result = div(10, 2);
+        let expected = Some(5);
+        assert_eq!(result, expected, "Numbers should be divisible");
+    }
+
+    #[test]
+    fn test_divide_none() {
+        let result = div(10, 0);
+        let expected = None;
+        assert_eq!(result, expected, "Numbers should not be divisible");
+    }
+
+    #[test]
+    fn test_concat() {
+        let result = concat("Hello", "World");
+        let expected = "HelloWorld".to_owned();
+        assert_eq!(result, expected, "str1 and str2 should be concated");
+    }
+}
